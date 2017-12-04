00478c95d8ae0447110221a860786f37ff3e856f
Bart De Nef <bartdn183@gmail.com>
2017-12-04 13:45:34 -0500

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,9 @@ class ApplicationController < ActionController::Base
 	   devise_parameter_sanitizer.permit(:sign_up, keys: [:locale, :time_zone, :first_name, :last_name, :role, :city, :country, :tel])
 	   devise_parameter_sanitizer.permit(:account_update, keys: [:locale, :time_zone, :first_name, :last_name, :role, :city, :country, :tel])
 	end
+
+  def after_sign_in_path_for(resource_or_scope)
+      blog_posts_path
+  end
+
 end
